b69ab2d4bed8d7903e4e3cb8d764b538a22cf0be
Simona <simona_s@outlook.com>
2023-03-03 22:46:55 -0800

    testing valid

--- script.js
+++ script.js
@@ -41,6 +41,8 @@
     var validLastname=false;
     var validEmail=false;
     var validPhone=false;
+    var validAddress=false;
+    var validCity=false;
     var validState=false;
     var validCountry=false;
     var validZipCode=false;
@@ -49,6 +51,8 @@
     var validLastname = document.getElementById("lastname").value;
     var userEmail = document.getElementById("email").value;
     var phone = document.getElementById("phone").value;
+    var address = document.getElementById("address").value;
+    var city = document.getElementById("city").value;
     var state = document.getElementById("state").value;
     var country = document.getElementById("country").value;
     var zipcode = document.getElementById("zipcode").value;
@@ -67,6 +71,12 @@
     if (isNaN(phone) || phone.lenght >15 || phone===null || phone===""){
         errorMessages += "<p>Invalid phone number</p>";
     }
+    if (address ===null){
+        errorMessages += "<p>Invalid address value</p>"
+    }
+    if (city ===null){
+        errorMessages += "<p>Invalid city value</p>"
+    }
     if (state ===null){
         errorMessages += "<p>Invalid state value</p>"
     }
@@ -78,6 +88,8 @@
         validLastname = true;
         validEmail = true;
         validPhone = true;
+        validAddress = true;
+        validCity = true;
         validState = true;
         validCountry = true;
         if (country === "USA"){
@@ -88,10 +100,11 @@
                 validZipCode = true;
             }
         }
+        errorMessages += "<p>Fields filled out successfully! Thank you!</p>"
     }
     //4) Send error message to HTML
     document.getElementById("errorMessages").innerHTML = errorMessages;
     //5) return status of each field
-    return (validFirstname && validLastname && validEmail && validPhone && validState && validCountry && validZipCode);
+    return (validFirstname && validLastname && validEmail && validPhone && validAddress && validCity && validState && validCountry && validZipCode);
     
 }
