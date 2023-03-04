326632058ae731665a9ce368f91471916d3b0eb1
Simona <simona_s@outlook.com>
2023-03-03 22:38:51 -0800

    check

--- script.js
+++ script.js
@@ -39,23 +39,59 @@
     //1) Create variable
     var validFirstname=false;
     var validLastname=false;
+    var validEmail=false;
+    var validPhone=false;
+    var validState=false;
+    var validCountry=false;
+    var validZipCode=false;
     //2) read value from HTML
     var firstname = document.getElementById("firstname").value;
     var validLastname = document.getElementById("lastname").value;
+    var userEmail = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var state = document.getElementById("state").value;
+    var country = document.getElementById("country").value;
+    var zipcode = document.getElementById("zipcode").value;
+    var atpos = userEmail.indexOf("@");
+    var dotpos = userEmail.lastIndexOf(".");
     //3) Do validation
     if (firstname==="null" || firstname==="" || firstname.length > 20){
         errorMessages += "<p>The firstname is required and cannot be greater than 20 characters</p>";
-    } else {
-        validFirstname = true;
     }
     if (lastname==="null" || lastname==="" || lastname.length > 20){
         errorMessages += "<p>The lastname is required and cannot be greater than 20 characters</p>";
-    } else {
+    } 
+    if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=userEmail.length) {
+        errorMessages += "<p>Invalid email</p>";
+    }
+    if (isNaN(phone) || phone.lenght >15 || phone===null || phone===""){
+        errorMessages += "<p>Invalid phone number</p>";
+    }
+    if (state ===null){
+        errorMessages += "<p>Invalid state value</p>"
+    }
+    if (country ===null){
+        errorMessages += "<p>Invalid country value</p>"
+    }
+    else {
+        validFirstname = true;
         validLastname = true;
+        validEmail = true;
+        validPhone = true;
+        validState = true;
+        validCountry = true;
+        if (country === "USA"){
+            if (isNaN(zipcode) || zipcode.lenght > 5 || zipcode===null || zipcode===""){
+                errorMessages += "<p>Invalid zipcode number</p>";
+            }
+            else {
+                validZipCode = true;
+            }
+        }
     }
     //4) Send error message to HTML
     document.getElementById("errorMessages").innerHTML = errorMessages;
     //5) return status of each field
-    return (validFirstname && validLastname);
+    return (validFirstname && validLastname && validEmail && validPhone && validState && validCountry && validZipCode);
     
 }
